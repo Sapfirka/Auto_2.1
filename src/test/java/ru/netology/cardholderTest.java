9aea98b5eccffccd4d89aba9ae556b0b9c04ac8d
Sapfirka <sapfirka@mail.ru>
2023-04-12 16:51:23 +0300

    add test

--- src/test/java/ru/netology/cardholderTest.java
+++ src/test/java/ru/netology/cardholderTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class cardholderTest {
     private WebDriver driver;
@@ -25,7 +29,18 @@ public class cardholderTest {
     }
 
     @Test
-    void shouldTestV1() {
+    void shouldTestV1() throws InterruptedException {
         driver.get("http:localhost:9999");
+
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Иванов Василий");
+        inputs.get(1).sendKeys ("+79778888888");
+        driver.findElement(By.className("checkbox__box")).click();
+        Thread.sleep(8000);
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
+        assertEquals(expected, actual);
     }
 }
+
